42d899ef29642d6dfb6457c61fd5a22383dde1ef
Swapnilabhyankar <swapnilabhyankar60@gmail.com>
2025-02-27 16:29:31 +0530

    File changes in last file

--- EligibleToVote.java
+++ EligibleToVote.java
@@ -14,6 +14,7 @@ public class EligibleToVote {
         {
             System.out.println("The person not eligible for voting");
         }
-
+        System.out.println("Change from Swapnil");
     }
+    
 }
